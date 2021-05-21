0a7af6ba041d554c6b9b3df73378aefcbbcfc238
Brent Lintner <brent.lintner@gmail.com>
2021-05-21 01:51:10 -0400

    Getting more things working for Rails 6, webpack, content policy stuff.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,20 +4,22 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
-#   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :data
-#   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
+Rails.application.config.content_security_policy do |p|
+  host = ENV['SERVER_HOSTS'].split(",").first || "localhost"
 
-#   # Specify URI for violation reports
-#   # policy.report_uri "/csp-violation-report-endpoint"
-# end
+  if Rails.env.development?
+    p.font_src    :self, :http, :data
+    p.default_src :self, :http, :unsafe_inline, :unsafe_eval
+    p.connect_src :self, :http, "http://#{host}:9000", "ws://#{host}:9000"
+  else
+    p.font_src    :self, :https, :data
+    p.script_src  :self, :https
+    p.default_src :self, :https
+  end
+end
 
 # If you are using UJS then enable automatic nonce generation
-# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
+Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
 
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
