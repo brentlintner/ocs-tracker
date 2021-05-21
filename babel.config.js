1ff56b5a68f9347c3a2ddb28597d0eeba63d3d1a
Brent Lintner <brent.lintner@gmail.com>
2021-05-20 20:09:43 -0400

    Fixes for making rails-webpacker work with latest.

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,50 @@
-module.exports = {
-  "presets": [
-    [
-      "@babel/env",
-      {
-        "modules": false,
-        "forceAllTransforms": true,
-        "targets": {
-          "browsers": "> 1%"
+module.exports = function(api) {
+  var validEnv = ['development', 'test', 'production']
+  var currentEnv = api.env()
+  var isDevelopmentEnv = api.env('development')
+  var isProductionEnv = api.env('production')
+  var isTestEnv = api.env('test')
+
+  if (!validEnv.includes(currentEnv)) {
+    throw new Error(
+      'Please specify a valid `NODE_ENV` or ' +
+        '`BABEL_ENV` environment variables. Valid values are "development", ' +
+        '"test", and "production". Instead, received: ' +
+        JSON.stringify(currentEnv) +
+        '.'
+    )
+  }
+
+  return {
+    presets: [
+      isTestEnv &&
+        [ '@babel/preset-env', { targets: { node: 'current' } }
+      ],
+      (isProductionEnv || isDevelopmentEnv) && [
+        '@babel/preset-env',
+        {
+          forceAllTransforms: true,
+          useBuiltIns: 'entry',
+          corejs: 3,
+          modules: false,
+          exclude: ['transform-typeof-symbol'],
+          targets: {
+            browsers: "> 1%"
+          }
         }
-      }
-    ],
-    "@babel/preset-react"
-  ],
-  "plugins": [
-    "@babel/plugin-proposal-object-rest-spread",
-    "@babel/plugin-syntax-dynamic-import",
-    [
-      "@babel/plugin-proposal-class-properties",
-      {
-        "spec": true
-      }
-    ]
-  ]
+      ],
+      [ "@babel/preset-react" ]
+    ].filter(Boolean),
+    plugins: [
+      'babel-plugin-macros',
+      '@babel/plugin-syntax-dynamic-import',
+      isTestEnv && 'babel-plugin-dynamic-import-node',
+      '@babel/plugin-transform-destructuring',
+      [ '@babel/plugin-proposal-class-properties', { loose: true, spec: true } ],
+      [ "@babel/plugin-proposal-private-methods", { "loose": true } ],
+      [ '@babel/plugin-proposal-object-rest-spread', { useBuiltIns: true } ],
+      [ '@babel/plugin-transform-runtime', { helpers: false } ],
+      [ '@babel/plugin-transform-regenerator', { async: false } ]
+    ].filter(Boolean)
+  }
 }
